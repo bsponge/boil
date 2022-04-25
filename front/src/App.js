be064a1fce251785fb611e3b789214f6de266aea
Jakub Skiba <kubask03@gmail.com>
2022-04-25 09:54:15 +0200

    Fix

--- front/src/App.js
+++ front/src/App.js
@@ -5,212 +5,213 @@ import TableData from './TableData'
 
 async function fetchGraph(graph) {
 
-  const response = await fetch('http://localhost:8080/', {
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*'
-    },
-    body: JSON.stringify(graph)
-  })
-  console.log("send")
-  //return await response.json()
+	const response = await fetch('http://localhost:8080/', {
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': '*'
+		},
+		body: JSON.stringify(graph)
+	})
+	console.log("send")
+	//return await response.json()
 
-  return {
-    nodes: [
-      // { id: "A", label: "Node 1", title: "node 1 tooltip text" },
-      // { id: "B", label: "Node 2", title: "node 2 tooltip text" },
-      // { id: "C", label: "Node 3", title: "node 3 tooltip text" },
-      // { id: "D", label: "Node 4", title: "node 4 tooltip text" },
-      // { id: "E", label: "Node 5", title: "node 5 tooltip text" },
-    ],
-    edges: [
-      { from: "A", to: "B"},
-      { from: "A", to: "C" },
-      { from: "B", to: "D" },
-      { from: "B", to: "E" },
-    ]
-  }
+	return {
+		nodes: [
+			// { id: "A", label: "Node 1", title: "node 1 tooltip text" },
+			// { id: "B", label: "Node 2", title: "node 2 tooltip text" },
+			// { id: "C", label: "Node 3", title: "node 3 tooltip text" },
+			// { id: "D", label: "Node 4", title: "node 4 tooltip text" },
+			// { id: "E", label: "Node 5", title: "node 5 tooltip text" },
+		],
+		edges: [
+			{ from: "A", to: "B"},
+			{ from: "A", to: "C" },
+			{ from: "B", to: "D" },
+			{ from: "B", to: "E" },
+		]
+	}
 }
 
 function Face(props) {
-  if (props.isEditor) {
-    return <TableData
-      graph={props.graph}
-      updateCallback={props.updateCallback}
-    />
-  } else {
-    //const response = fetchGraph(props.graph)
-    return <Graph
-      graph={props.graph}
-      options={props.options}
-      events={props.events}
-      style={{ height: "800px" }}
-    />
-  }
+	if (props.isEditor) {
+		return <TableData
+		graph={props.graph}
+		updateCallback={props.updateCallback}
+			/>
+	} else {
+		//const response = fetchGraph(props.graph)
+		return <Graph
+		graph={props.graph}
+		options={props.options}
+		events={props.events}
+		style={{ height: "800px" }}
+			/>
+	}
 }
 
 var cpmLabels = []
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props)
 
-    this.state = {
-      editor: true,
-      graph: {
-        nodes: [
-          // { id: "A", label: "Node 1", title: "node 1 tooltip text" },
-          // { id: "B", label: "Node 2", title: "node 2 tooltip text" },
-          // { id: "C", label: "Node 3", title: "node 3 tooltip text" },
-          // { id: "D", label: "Node 4", title: "node 4 tooltip text" },
-          // { id: "E", label: "Node 5", title: "node 5 tooltip text" },
-        ],
-        edges: [
-          // { from: "A", to: "B", label: "JFKDSLFDS", color: "red" },
-          // { from: "A", to: "C" },
-          // { from: "B", to: "D" },
-          // { from: "B", to: "E" },
-        ]
-      }
-    }
+		this.state = {
+			editor: true,
+			graph: {
+				nodes: [
+					// { id: "A", label: "Node 1", title: "node 1 tooltip text" },
+					// { id: "B", label: "Node 2", title: "node 2 tooltip text" },
+					// { id: "C", label: "Node 3", title: "node 3 tooltip text" },
+					// { id: "D", label: "Node 4", title: "node 4 tooltip text" },
+					// { id: "E", label: "Node 5", title: "node 5 tooltip text" },
+				],
+				edges: [
+					// { from: "A", to: "B", label: "JFKDSLFDS", color: "red" },
+					// { from: "A", to: "C" },
+					// { from: "B", to: "D" },
+					// { from: "B", to: "E" },
+				]
+			}
+		}
 
-    this.edit = this.edit.bind(this)
-    this.updateGraph = this.updateGraph.bind(this)
-  }
+		this.edit = this.edit.bind(this)
+		this.updateGraph = this.updateGraph.bind(this)
+	}
 
-  // convert activities to graph
-  convertNodes(activityArray){
-  
-    // convert undefinded labels to string
-    for(var i = 0; i < cpmLabels.length; i++) String(cpmLabels[i])
+	// convert activities to graph
+	convertNodes(activityArray){
 
-    this.graph = {
-      edges: [],
-      nodes:[]
-    }
+		// convert undefinded labels to string
+		for(var i = 0; i < cpmLabels.length; i++) String(cpmLabels[i])
 
-    // add nodes to graph
-    var nodeList = []
-    for(var i = 0; i < activityArray.length; i++){
-      if(!nodeList.includes(activityArray[i].destination)){
-        nodeList.push(activityArray[i].destination)
-        var node = { id: activityArray[i].destination, label: activityArray[i].destination}
-        this.graph.nodes.push(node)
-      }
-      
-      if(!nodeList.includes(activityArray[i].source)){
-        nodeList.push(activityArray[i].source)
-        var node = { id: activityArray[i].source, label: activityArray[i].source}
-        this.graph.nodes.push(node)
-      }
-    }
+		let graph = {
+			edges: [],
+			nodes:[]
+		}
 
-    for(var i = 0; i < cpmLabels.length; i++) console.log( cpmLabels[i])
-    for(var i = 0; i < activityArray.length; i++){
-      var col
-      var checker = String(activityArray[i].action)
+		// add nodes to graph
+		var nodeList = []
+		for(var i = 0; i < activityArray.length; i++){
+			if(!nodeList.includes(activityArray[i].destination)){
+				nodeList.push(activityArray[i].destination)
+				var node = { id: activityArray[i].destination, label: activityArray[i].destination}
+				graph.nodes.push(node)
+			}
 
-      if(cpmLabels.includes(checker))
-        col = "red"
-        else
-        col = "black"
+			if(!nodeList.includes(activityArray[i].source)){
+				nodeList.push(activityArray[i].source)
+				var node = { id: activityArray[i].source, label: activityArray[i].source}
+				graph.nodes.push(node)
+			}
+		}
 
-      var edge = { from: activityArray[i].source, to: activityArray[i].destination, label: activityArray[i].action+activityArray[i].duration, color: col}
-      this.graph.edges.push(edge)
-    }
+		for(var i = 0; i < cpmLabels.length; i++) console.log( cpmLabels[i])
+		for(var i = 0; i < activityArray.length; i++){
+			var col
+			var checker = String(activityArray[i].action)
 
-    
-    return this.graph
-  }
-  
-
-  edit(event) {
-    
-    const data = JSON.stringify(this.state.graph) // save data to post
-
-    JSON.stringify(data)
-    console.log("SENDING: "+data)
-    // this.state.graph = this.convertNodes(this.state.graph)
-    
-    // const cpmLabels = [] // labels of CPM edges
-    // get CPM from server alghoritm
-    fetch('http://localhost:8080/',{
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json',
-      },
-      body: data
-    })
-    .then((response)=>response.json())
-    .then((data)=>{
-      // var cpmLabels = [] // labels of CPM edges
-      console.log('SUCCES', data)
-      JSON.stringify(data)
-      for(var i = 0; i < data.length; i++){
-          if(data[i].from != data[i].to){
-            console.log("dodaje wartosc")
-            console.log(data[i].label)
-            console.log(typeof(data[i].label))
-            cpmLabels.push(String(data[i].label))
-            // cpmLabels[i] = String(data[i].label)
-            console.log(typeof(cpmLabels[i]))
-          }
-      }
-      console.log('SUCCESS 2')
-      console.log(cpmLabels)
-      // this.graph = this.state.graph
-      // this.state.graph = this.convertNodes(this.state.graph,cpmLabels)
-      
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-
-    this.state.graph = this.convertNodes(this.state.graph)
-    this.setState({editor: !this.state.editor})
-
-  }
+			var edge = { from: activityArray[i].source, to: activityArray[i].destination, label: activityArray[i].action+activityArray[i].duration, color: activityArray[i].color}
+			graph.edges.push(edge)
+		}
 
 
-  updateGraph(graph) {
-    console.log("UPDATING GRAPH")
-    console.log(graph)
-    var prev = {...this.state}
-    prev.graph = graph
-    this.setState(prev)
-  }
+		return graph
+	}
 
 
-  render() {
-    const options = {
-      edges: {
-        color: "#000000"
-      },
-    }
+	edit(event) {
 
-    const events = {
-      select: function (event) {
-        var { nodes, edges } = event
-      }
-    }
+		const data = JSON.stringify(this.state.graph) // save data to post
+		console.log("PRINT GRAPH BEFORE POST")
+		console.log(this.state.graph)
 
-    return (
-      <div>
-        <div class="btn btn-three" onClick={this.edit}>
-          <span>Animate</span>
-        </div>
-        <Face
-          isEditor={this.state.editor}
-          graph={this.state.graph}
-          options={options}
-          events={events}
-          updateCallback={this.updateGraph}
-        />
-      </div>
-    );
-  }
+		JSON.stringify(data)
+		console.log("SENDING: "+data)
+		// this.state.graph = this.convertNodes(this.state.graph)
+
+		// const cpmLabels = [] // labels of CPM edges
+		// get CPM from server alghoritm
+		fetch('http://localhost:8080/',{
+			method: 'POST',
+			headers:{
+				'Content-Type': 'application/json',
+			},
+			body: data
+		})
+			.then((response)=>response.json())
+			.then((data)=>{
+				// var cpmLabels = [] // labels of CPM edges
+				console.log('SUCCES', data)
+				JSON.stringify(data)
+				for(var i = 0; i < data.length; i++){
+					if(data[i].from != data[i].to){
+						cpmLabels.push(String(data[i].label))
+						for (var j = 0; j < this.state.graph.length; j++) {
+							if (this.state.graph[j].action == data[i].label) {
+								this.state.graph[j].color = "red"
+								console.log("JEST")
+							}
+						}
+					}
+				}
+
+				this.state.graph = this.convertNodes(this.state.graph)
+
+				this.setState({editor: !this.state.editor})
+				console.log("RECEIVED CPM DATA")
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+
+		console.log("PRINT GRAPH =========")
+		console.log(this.state.graph)
+
+
+
+
+	}
+
+
+	updateGraph(graph) {
+		console.log("UPDATING GRAPH")
+		console.log(graph)
+		var prev = {...this.state}
+		prev.graph = graph
+		this.setState(prev)
+	}
+
+
+	render() {
+		const options = {
+			edges: {
+				color: "#000000"
+			},
+		}
+
+		const events = {
+			select: function (event) {
+				var { nodes, edges } = event
+			}
+		}
+
+		return (
+			<div>
+			<div class="btn btn-three" onClick={this.edit}>
+			<span>Animate</span>
+			</div>
+			<Face
+			isEditor={this.state.editor}
+			graph={this.state.graph}
+			options={options}
+			events={events}
+			updateCallback={this.updateGraph}
+			/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
+	"github.com/bsponge/boil/event"
 	"github.com/bsponge/boil/graph"
+	"github.com/bsponge/boil/types"
 )
 
 // "net/http"
@@ -13,84 +19,89 @@ import (
 
 func main() {
 
-	fmt.Println("test")
-	g := graph.New(4)
-
-	g.AddEdge(0, 1, 5, "A")
-	g.AddEdge(0, 2, 7, "B")
-	g.AddEdge(1, 3, 6, "C")
-	g.AddEdge(1, 4, 8, "D")
-	g.AddEdge(2, 4, 3, "E")
-	g.AddEdge(3, 4, 4, "F")
-	g.AddEdge(3, 5, 2, "G")
-	g.AddEdge(4, 5, 5, "H")
 	/*
+		fmt.Println("test")
+		g := graph.New(4)
 
-		g.AddEdge(0, 1, 1, "A")
-		g.AddEdge(1, 2, 1, "B")
-		g.AddEdge(2, 3, 4, "C")
-		g.AddEdge(3, 4, 1, "D")
+		g.AddEdge(0, 1, 5, "A")
+		g.AddEdge(0, 2, 7, "B")
+		g.AddEdge(1, 3, 6, "C")
+		g.AddEdge(1, 4, 8, "D")
+		g.AddEdge(2, 4, 3, "E")
+		g.AddEdge(3, 4, 4, "F")
+		g.AddEdge(3, 5, 2, "G")
+		g.AddEdge(4, 5, 5, "H")
+
+			g.AddEdge(0, 1, 1, "A")
+			g.AddEdge(1, 2, 1, "B")
+			g.AddEdge(2, 3, 4, "C")
+			g.AddEdge(3, 4, 1, "D")
+
+		path := g.CPM()
+
+		fmt.Println(g.String())
+
+		fmt.Println(path)
 	*/
 
-	path := g.CPM()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 
-	fmt.Println(g.String())
+		if r.Method == "POST" {
+			// actions := []event.Nodes{}
+			parser := []event.Parser{}
+			fmt.Println("got!")
 
-	fmt.Println(path)
-
-	/*
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "*")
-
-			if r.Method == "POST" {
-				// actions := []event.Nodes{}
-				parser := []event.Parser{}
-				fmt.Println("got!")
-
-				// err := json.NewDecoder(r.Body).Decode(&actions)
-				err := json.NewDecoder(r.Body).Decode(&parser)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-
-				fmt.Println("printing info:")
-				g := graph.New(len(parser))
-
-				println("PARSER TEST:  ", parser[0].Destination, " label = ", parser[0].Action)
-				println("PARSER TEST:  ", parser[1].Destination, " label = ", parser[1].Action)
-
-				fmt.Println(parser)
-
-				for _, pr := range parser {
-					// fmt.Println("loooop: source ", action.Source, " dest: ", action.Destination, " time: ", action.Duration, " l = ", action.Label)
-					source, err := strconv.ParseUint(pr.Source, 10, 32)
-					destination, err1 := strconv.ParseUint(pr.Destination, 10, 32)
-
-					duration, err2 := strconv.ParseUint(pr.Duration, 10, 32)
-					// println("parsed: cost = ", duration)
-
-					// g.AddEdge(action.Source, action.Destination, action.Duration, action.Label)
-					if err != nil && err1 != nil && err2 != nil {
-						fmt.Println("Error caught!")
-					} else {
-						g.AddEdge(uint(source), uint(destination), types.Cost(duration), pr.Action)
-					}
-
-				}
-
-				cpm := g.CPM()
-				fmt.Print(cpm)
-				w.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(w).Encode(cpm)
+			// err := json.NewDecoder(r.Body).Decode(&actions)
+			err := json.NewDecoder(r.Body).Decode(&parser)
+			if err != nil {
+				log.Println(err)
+				return
 			}
-		})
 
-		err := http.ListenAndServe(":8080", nil)
-		if err != nil {
-			log.Fatal("HTTP server error", err)
-			return
+			fmt.Println("printing info:")
+			var biggest uint64 = 0
+			for i := range parser {
+				d, _ := strconv.ParseUint(parser[i].Destination, 10, 32)
+				if d > biggest {
+					biggest = d
+				}
+			}
+			g := graph.New(int(biggest) - 1)
+
+			println("PARSER TEST:  ", parser[0].Destination, " label = ", parser[0].Action)
+			println("PARSER TEST:  ", parser[1].Destination, " label = ", parser[1].Action)
+
+			fmt.Println(parser)
+
+			for _, pr := range parser {
+				// fmt.Println("loooop: source ", action.Source, " dest: ", action.Destination, " time: ", action.Duration, " l = ", action.Label)
+				source, err := strconv.ParseUint(pr.Source, 10, 32)
+				destination, err1 := strconv.ParseUint(pr.Destination, 10, 32)
+
+				duration, err2 := strconv.ParseUint(pr.Duration, 10, 32)
+				// println("parsed: cost = ", duration)
+
+				// g.AddEdge(action.Source, action.Destination, action.Duration, action.Label)
+				if err != nil && err1 != nil && err2 != nil {
+					fmt.Println("Error caught!")
+				} else {
+					g.AddEdge(uint(source), uint(destination), types.Cost(duration), pr.Action)
+				}
+
+			}
+
+			cpm := g.CPM()
+			fmt.Print(cpm)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(cpm)
 		}
-	*/
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("HTTP server error", err)
+		return
+	}
 }
